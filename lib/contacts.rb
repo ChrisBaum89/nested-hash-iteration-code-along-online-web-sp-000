fb22628a8ed64c335068ffe828084e62b317fe1d
Chris Baum <christopher.baum@valpo.edu>
2021-01-18 02:02:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if person == "Freddy Mercury"
-        contact_details_hash.each do |attribute, data|
+        value.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
